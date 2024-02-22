edfe37acd4cecd3f9d207acf618e5dbae8308e0f
boburbo <bobur20081608@gmail.com>
2024-02-22 18:49:16 +0500

    vazifa4.5

---
+++ app.js
@@ -0,0 +1,92 @@
+// let son1 = +prompt('birinchi sonni kiriting')
+// let son2 = +prompt(`ikkinchi sonni kiriting`)
+
+// function sum(a,b) {
+//     result = a + b 
+//     console.log(result);
+// } 
+// sum(son1,son2);
+
+//N2
+
+// let a =+prompt('son kiritin')
+
+// function secund(a) {
+//     result = a*60
+//     alert(result + ` sekund`);
+    
+// }
+// secund(a);
+
+// N3
+
+// let a =+prompt('son kiritin')
+// function nextInteger(a) {
+//      a++
+//     alert(a)
+// }
+
+// nextInteger(a)
+
+// N4
+
+// let a =+prompt('son kiritin')
+
+// function tomon(a) {
+//     result = a * a
+//     alert(a)
+// }
+// tomon(a)
+//N5
+
+// let a= 20;
+
+
+// function vaqt(a) {
+//  result = a * 365
+//  console.log(result);
+    
+// }
+// vaqt(a)
+
+
+
+// let a = 3;
+
+// function kub(b) {
+//     result = b **3
+//     console.log(result);
+// }
+// kub(a)
+
+
+//  n8 Ikkita sonni kupaytmasi
+
+// let num1 = 15
+// let num2 = 25
+// let result
+
+// function power(a,b){
+//     result = num1 * num2
+//     console.log(result)
+// }
+
+// power(num1,num2)
+
+
+
+//N9
+
+
+// //
+// let soat = +prompt("Soatni kiriting")
+// let result
+ 
+// function sekund(a){
+//     result = a * 60
+//     result = result * 60
+//     console.log(result + "sekund")
+// }
+
+// sekund(soat)
+// //
